b404bd4ce4e6b949526ad571e914a42e32c3f632
Steven Elleman <selleman@berkeley.edu>
2021-11-03 16:06:22 -0700

    soft commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CytoscapeComponent from 'react-cytoscapejs';
+
+class App extends React.Component {
+  constructor(props) {
+    const queryParams = new URLSearchParams(window.location.search);
+
+    super(props);
+    this.state = {
+      nodes: queryParams.get("nodes") || 0,
+      value: -1,
+      elements: [
+        {data: {id: 'one', label: 'Node 1'}, position: {x: 30, y: 30}},
+        {data: {id: 'two', label: 'Node 2'}, position: {x: 130, y: 30}},
+        {data: {source: 'one', target: 'two', label: 'Edge from Node1 to Node2'}}
+      ]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  /*
+  Text Fields:
+  - Nodes
+  - Odds
+
+  Output:
+  - Odds of collision somewhere
+
+  TODO:
+  1. Add text fields
+  2. Center style cytoscape component
+  3. Autogenerate number of elements
+    - Need to calculate positions, which should be fun
+    - Need to calculate edges, which should also be fun
+
+  Notes:
+  - Margin of 30px, about
+   */
+
+  calculateCollision() {
+    // Calculate the odds of a collision
+    // Calculate the odds of no collision: 1 - (1 - collision rate)^(total number of connections)
+  }
+
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    console.log(window, window.location)
+  }
+
+  handleSubmit(event) {
+
+    /*
+    TODO:
+    - Calculate positions in 600 x 600 grid
+      - Picture circumscribing a circle by n
+      - Diameter of 500
+      1. Calculate based on center, and just add offset of (300, 300)
+      2. Need to go from circumference to position, any way to use polar coordinates?
+        - Just use degrees, should be sufficient,
+     */
+
+    if (this.state.value > 0 && this.state.value < 100) {
+      this.setState({
+        nodes: this.state.value,
+        elements: []
+      });
+    } else {
+      alert("Value must be greater than 1 and less than 100")
+    }
+  }
+
+  render() {
+    return (
+      <div style={{width:"100%", height: "100%", padding: "50px"}}>
+        <form>
+          <label>
+            Current Nodes: {this.state.nodes}
+            <input type="number" name="nodes" onChange={this.handleChange} />
+          </label>
+          <label>
+            Collision Rate: {this.state.collision_rate}
+            <input type="number" name="collision_rate" onChange={this.handleChange} />
+          </label>
+          <label>
+            Connections: {this.state.connections}
+          </label>
+
+
+          <input type="submit" value="Submit" onClick={this.handleSubmit} />
+        </form>
+
+        <CytoscapeComponent elements={this.state.elements} style={{width: '600px', height: '600px'}}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -4,11 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//   </React.StrictMode>,
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
